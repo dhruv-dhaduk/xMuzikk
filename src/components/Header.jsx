6072c6b34c1aa86d89b75887a31a8b98f2c61261
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-10 19:46:18 +0530

    test commit in header branch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import logo from '/logos/xMuzikk.png';
 
 function Header({ className = 'h-12 tablet:h-14' }) {
     return (
-        <header className={`bg-black flex justify-start border-b border-slate-900 backdrop-blur-md bg-opacity-65 ${className}`}>
+        <header 
+            className={`bg-black flex justify-start border-b border-slate-900 backdrop-blur-md bg-opacity-65 ${className}`}
+        >
             <div className='w-fit h-full p-2.5 flex justify-start items-center tablet:p-3'>
                 <img 
                     src={logo}
