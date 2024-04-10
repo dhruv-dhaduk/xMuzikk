418d4b1f4a4ebcb08282027dae568441990b193e
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-10 20:07:34 +0530

    Added the hover effect to logo if it is not mobile device

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import logo from '/logos/xMuzikk.png';
+import isMobileDevice from '../utils/isMobileDevice.js';
 
 function Header({ className = 'h-12 tablet:h-14' }) {
     return (
         <header 
             className={`bg-black flex justify-start border-b border-slate-900 backdrop-blur-md bg-opacity-65 ${className}`}
         >
-            <div className='w-fit h-full p-2.5 flex justify-start items-center cursor-pointer tablet:p-3 active:bg-slate-800'>
+            <div className={`w-fit h-full p-2.5 flex justify-start items-center cursor-pointer tablet:p-3 active:bg-slate-800 ${!isMobileDevice ? 'hover:bg-slate-900' : ''}`}>
                 <img 
                     src={logo}
                     draggable={false}
