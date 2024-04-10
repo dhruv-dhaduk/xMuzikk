28676adeaf7419a32226368197a39a88f040dd75
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-10 19:59:43 +0530

    Added the active effect to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header({ className = 'h-12 tablet:h-14' }) {
         <header 
             className={`bg-black flex justify-start border-b border-slate-900 backdrop-blur-md bg-opacity-65 ${className}`}
         >
-            <div className='w-fit h-full p-2.5 flex justify-start items-center tablet:p-3'>
+            <div className='w-fit h-full p-2.5 flex justify-start items-center cursor-pointer tablet:p-3 active:bg-slate-800'>
                 <img 
                     src={logo}
                     draggable={false}
