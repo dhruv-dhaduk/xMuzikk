5ca91a6b2b95360de8ea5fd6d73777a34ffc7952
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-10 19:53:25 +0530

    Added the Header component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Header from "./components/Header";
+
 function App() {
     return (
-        <>
-            <div>xMuzikk</div>
+        <>  
+            <Header className='w-full h-12 fixed top-0 inset-x-0 tablet:h-14'/>
+            
+            <main className='mt-12 tablet:mt-14'>
+                <div>xMuzikk</div>
+            </main>
+
         </>
     );
 }
