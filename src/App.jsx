868ed8b9b91d26d106faaa602bbe165c054e7c94
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-12 14:57:53 +0530

    Removed the bug in providing PlayerContext

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     return (
         <>  
 
-            <PlayerContext.Provider value={isPlayerShowing}>
+            <PlayerContext.Provider value={{isPlayerShowing}}>
                 <Header className='w-full h-12 fixed top-0 inset-x-0 tablet:h-14'/>
                 
                 <main className='mt-12 mb-12 tablet:mt-14 tablet:mb-0 tablet:ml-36'>
