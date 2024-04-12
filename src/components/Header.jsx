8d841b6629737e408a780e2068e9304a9ac840a9
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-12 15:19:10 +0530

    Added the navigation to home to header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,31 @@
 import logo from '/logos/xMuzikk.png';
 import isMobileDevice from '../utils/isMobileDevice.js';
 
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PlayerContext } from '../contexts/PlayerContext.js';
+
 function Header({ className }) {
+
+    const playerContext = useContext(PlayerContext);
+    const navigate = useNavigate();
+
+    const gotoHome = () => {
+        if (playerContext.isPlayerShowing)
+            return;
+
+        navigate('/');
+        window.scrollTo(0, 0);
+    }
+    
     return (
         <header 
             className={`bg-black z-50 flex justify-start border-b border-slate-900 backdrop-blur-md bg-opacity-65 ${className}`}
         >
-            <div className={`w-fit h-full p-2.5 flex justify-start items-center cursor-pointer tablet:p-3 active:bg-slate-800 ${!isMobileDevice ? 'hover:bg-slate-900' : ''}`}>
+            <div 
+                className={`w-fit h-full p-2.5 flex justify-start items-center cursor-pointer tablet:p-3 active:bg-slate-800 ${!isMobileDevice ? 'hover:bg-slate-900' : ''}`}
+                onClick={gotoHome}    
+            >
                 <img 
                     src={logo}
                     draggable={false}
