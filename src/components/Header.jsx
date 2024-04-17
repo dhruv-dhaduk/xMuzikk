ddd22d522c522d0e31f3c7e6c425708a3eaafa6e
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-17 13:28:09 +0530

    Changed the bg blur filter and opacity on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header({ className }) {
     
     return (
         <header 
-            className={`bg-black z-50 flex justify-start border-b border-slate-900 backdrop-blur-md bg-opacity-65 ${className}`}
+            className={`bg-black z-50 flex justify-start border-b border-slate-900 backdrop-blur-[8px] bg-opacity-50 ${className}`}
         >
             <div 
                 className={`w-fit h-full p-2.5 flex justify-start items-center cursor-pointer tablet:p-3 active:bg-slate-800 ${!isMobileDevice ? 'hover:bg-slate-900' : ''}`}
