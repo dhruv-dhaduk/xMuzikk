b404088b4647e9316ee58f47a22352a4dd05ce5e
dhruv-dhaduk <dhaduk44@gmail.com>
2024-05-08 17:38:05 +0530

    Fixed the bug in PlayerPage's hide and show animation

--- src/pages/PlayerPage.jsx
+++ src/pages/PlayerPage.jsx
@@ -2,25 +2,33 @@ import { useEffect, useRef, useState } from "react";
 
 function PlayerPage({ isPlayerShowing, hidePlayer, className }) {
     const [count, setCount] = useState(0);
+    const containerRef = useRef(null);
     const isFirstRender = useRef(true);
+
     useEffect(() => {
-        if (isFirstRender.current)
-            isFirstRender.current = false;
+        isFirstRender.current = true;
     }, []);
 
-    let showHideClasses = '';
-    if (isPlayerShowing) {
-        if (!isFirstRender.current) 
-            showHideClasses += 'animate-show';
-        showHideClasses += ' top-0';
-    }
-    else {
-        if (!isFirstRender.current)
-            showHideClasses += 'animate-hide';
-        showHideClasses += ' top-out';
-    }
+    useEffect(() => {
+        if (!isFirstRender.current) {
+            if (isPlayerShowing) {
+                containerRef.current.classList.remove('animate-hide');
+                containerRef.current.classList.add('animate-show');
+            }
+            else {
+                containerRef.current.classList.remove('animate-show');
+                containerRef.current.classList.add('animate-hide');
+            }
+        }
+
+        isFirstRender.current = false;
+    }, [isPlayerShowing]);
+
     return (
-        <div className={`bg-black w-dvw h-dvh fixed p-4 inset-x-0 ${showHideClasses} ${className}`}>
+        <div 
+            ref={containerRef}
+            className={`bg-black w-dvw h-dvh fixed p-4 inset-x-0 ${isPlayerShowing ? 'top-0' : 'top-out'} ${className}`}    
+        >
             <div className='h-full w-full border border-white'>
                 <button 
                     className='bg-white text-black font-bold rounded p-4'
