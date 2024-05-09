585703cf8e930b69acaab80731f81ce451781c67
dhruv-dhaduk <dhaduk44@gmail.com>
2024-05-09 14:03:14 +0530

    Added buttons and titles in Player Page

--- src/pages/PlayerPage.jsx
+++ src/pages/PlayerPage.jsx
@@ -4,6 +4,7 @@ import { PlayerContext } from '../contexts/PlayerContext.js';
 import closeIcon from '/icons/close.svg';
 import shareIcon from '/icons/share.svg';
 import youtubeIcon from '/icons/youtube.svg';
+import heartHollowIcon from '/icons/heart_hollow.svg';
 
 function PlayerPage({ isPlayerShowing, hidePlayer, className }) {
     const containerRef = useRef(null);
@@ -44,7 +45,7 @@ function PlayerPage({ isPlayerShowing, hidePlayer, className }) {
             </div>
             <div className='p-8 w-full h-fit max-h-full bg-opacity-45 overflow-y-scroll tablet:bg-white tablet:backdrop-blur-[35px] tablet:bg-opacity-10'>
                 
-                <div className='tablet:flex aspect-[2] h-full gap-8'>
+                <div className='tablet:flex aspect-[2] h-full gap-5 w-full'>
 
                     <div className='aspect-square'>
                         <div className='bg-slate-700 w-full h-full overflow-hidden relative'>
@@ -57,15 +58,49 @@ function PlayerPage({ isPlayerShowing, hidePlayer, className }) {
                         </div>
                     </div>
 
-                    <div className='flex-1'>
-                        <img src={closeIcon} className='w-10 h-10' />
-                        <img src={shareIcon} className='w-10 h-10' />
-                        <img src={youtubeIcon} className='w-10 h-10' />
+                    <div className='flex-1 bg-blackk'>
+                        <div className='flex justify-start items-center w-full'>
+                            <Icon imgSrc={youtubeIcon} className='w-16 p-4 rounded-full' />
+                            <Icon imgSrc={shareIcon} className='w-16 p-4 rounded-full' />
+                            <Icon imgSrc={closeIcon} className='ml-auto w-16 p-2 rounded-full bg-white bg-opacity-25' />
+                        </div>
+
+                        <div className='flex items-start justify-between mt-8'>
+                            <div className='flex-1'>
+                                <p className='line-clamp-2 text-xl font-bold mb-1.5'>
+                                    { title }
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta maxime possimus placeat. Voluptates, blanditiis quae. Hic vitae aspernatur doloremque saepe?
+                                </p>
+
+                                <p className='line-clamp-1 text-sm'>
+                                    { channelTitle }
+                                    <span className='mx-2 font-bold'>·</span>
+                                    3 years ago
+                                </p>
+                            </div>
+
+                            <Icon imgSrc={heartHollowIcon} className='flex-none w-16 p-2' />
+                        </div>
+
+                        <div>
+                            
+                        </div>
                     </div>
 
                 </div>
 
             </div>
+        </div>
+    );
+}
+
+function Icon({ imgSrc, className }) {
+    return (
+        <div className={`aspect-square ${className}`}>
+            <img 
+                src={imgSrc}
+                className='w-full h-full object-cover'
+            />
         </div>
     );
 }
