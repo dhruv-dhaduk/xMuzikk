808715af82b0aa65e556581c213acb2fb4a5e417
dhruv-dhaduk <dhaduk44@gmail.com>
2024-05-10 16:28:48 +0530

    Used the LoadingFeed component in Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,8 +2,14 @@ import FeedItem from "./FeedItem";
 import { PlayerContext } from '../contexts/PlayerContext.js';
 import { useContext } from "react";
 
+import LoadingFeed from "./Loading.jsx";
+
 function Feed({ musicList }) {
     const playerContext = useContext(PlayerContext);
+
+    if (!musicList || !musicList.length) {
+        return <LoadingFeed count={12} />
+    }
 
     return (
         <div className='grid grid-cols-1 tablet:grid-cols-feed tablet:gap-x-6 target:gap-y-12 tablet:p-6'>
