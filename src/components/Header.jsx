07a9e95396ea9dbd092886f43dfe08939b205760
Dhruv Dhaduk <dhadukd44@gmail.com>
2024-05-18 15:13:46 +0530

    Moved the site logo back to the left in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ function Header({ className }) {
     
     return (
         <header 
-            className={`flex justify-center tablet:justify-start bg-black backdrop-blur-[8px] bg-opacity-50 border-b border-slate-900 ${className}`}
+            className={`flex justify-start bg-black backdrop-blur-[8px] bg-opacity-50 border-b border-slate-900 ${className}`}
         >
             <div 
                 className={`w-fit h-full flex justify-start items-center p-2.5 tablet:p-3 active:bg-slate-800 cursor-pointer ${!isMobileDevice ? 'hover:bg-slate-900' : ''}`}
