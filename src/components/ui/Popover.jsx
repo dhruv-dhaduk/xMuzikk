d7d795b0fcec9e8a2bbf41b8a00144b1022e0222
dhruv-dhaduk <dhadukd44@gmail.com>
2024-06-02 15:42:19 +0530

    Created a Popover component

---
+++ src/components/ui/Popover.jsx
@@ -0,0 +1,41 @@
+import { useCallback } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+
+function Popover({ popoverShowing, setPopoverShowing, children, className }) {
+    const popoverRef = useRef(null);
+
+    useEffect(() => {
+        if (popoverShowing === true)
+            popoverRef.current.showPopover();
+        else if (popoverShowing === false)
+            popoverRef.current.hidePopover();
+    }, [popoverShowing]);
+
+    const changePopoverShowingState = useCallback((e) => {
+        if (e.newState === 'open')
+            setPopoverShowing(true);
+        else if (e.newState === 'closed')
+            setPopoverShowing(false);
+    }, [setPopoverShowing]);
+
+    useEffect(() => {
+        popoverRef.current.addEventListener('toggle', changePopoverShowingState);
+
+        return () => {
+            popoverRef.current.removeEventListener('toggle', changePopoverShowingState);
+        }
+    }, [changePopoverShowingState]);
+
+    return (
+        <div
+            popover='auto'
+            ref={popoverRef}
+            className={className}
+        >
+            { children }
+        </div>
+    );
+}
+
+export default Popover;
