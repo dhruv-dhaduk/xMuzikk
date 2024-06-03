e86fa8c1fcf3f032f9f5df1cc03c73ea45eaccf1
dhruv-dhaduk <dhadukd44@gmail.com>
2024-06-03 23:02:11 +0530

    Added Sign Up functionality

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,6 +1,76 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { authService } from '../dataManager/AppwriteService.js';
 
 function SignupPage() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const [submitDisabled, setSubmitDisabled] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        
+        if (!name?.length) {
+            alert('Please Enter Your Name');
+            return;
+        }
+        
+        if (!email?.length) {
+            alert('Please Enter Your Email');
+            return;
+        }
+        
+        if (!password?.length) {
+            alert('Please Enter the Password');
+            return;
+        }
+        
+        if (!confirmPassword?.length) {
+            alert('Please Confirm the Password');
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            alert(`Confirm Password doesn't match the Password.`);
+            return;
+        }
+
+        if (password.length < 8) {
+            alert('Password must be atleast 8 characters long.');
+            return;
+        }
+
+        setSubmitDisabled(true);
+
+        authService
+            .createAccount(email, password, name)
+            .then(({response, error}) => {
+                if (error) {
+                    console.log(error);
+                    alert(`Error : ${error}`);
+                    return;
+                }
+                else {
+                    console.log(response);
+                    navigate('/login');
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                alert(`Error : ${err}`);
+            })
+            .finally(() => {
+                setSubmitDisabled(false);
+            });
+    }
+    
     return (
         <div className='flex justify-center p-4'>
             <div className='bg-card border border-card w-full tablet:max-w-[30rem] rounded-3xl'>
@@ -10,14 +80,15 @@ function SignupPage() {
 
                 <form
                     className='flex flex-col gap-4 px-4 pt-6'
-                    onSubmit={e => e.preventDefault()}
+                    onSubmit={handleSubmit}
                 >
 
                     <div>
                         <Label>Your Name</Label>
                         <input
-                            type="text"
-                            name='name'
+                            type='text'
+                            value={name}
+                            onChange={e => setName(e.target.value)}
                             placeholder='Enter Your Name'
                             className='input-text w-full'
                         />
@@ -27,7 +98,8 @@ function SignupPage() {
                         <Label>Your Email</Label>
                         <input
                             type='email'
-                            name='email'
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
                             placeholder='Enter Your Email'
                             className='input-text w-full'
                         />
@@ -37,7 +109,8 @@ function SignupPage() {
                         <Label>Create Password</Label>
                         <input
                             type='password'
-                            name='password'
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
                             placeholder='Enter a Password'
                             className='input-text w-full'
                         />
@@ -47,17 +120,27 @@ function SignupPage() {
                         <Label>Confirm Password</Label>
                         <input
                             type='password'
-                            name='password'
+                            value={confirmPassword}
+                            onChange={e => setConfirmPassword(e.target.value)}
                             placeholder='Confirm your Password'
                             className='input-text w-full'
                         />
+                        <p className='text-[12px]'>
+                            {
+                                (confirmPassword.length && confirmPassword === password) ? <span className='text-green-500'>(Password matches)</span> : ''
+                            }
+                            {
+                                (confirmPassword.length && confirmPassword !== password) ? <span className='text-red-500'>(Password doesn't match)</span> : ''
+                            }
+                        </p>
                     </div>
 
                     <div className='mt-4'>
                         <input
                             type='submit'
                             value='Create Account'
-                            className='w-full h-10 bg-white text-black font-bold rounded-full cursor-pointer active:bg-opacity-80'
+                            disabled={submitDisabled}
+                            className='w-full h-10 bg-white text-black font-bold rounded-full cursor-pointer active:bg-opacity-80 disabled:bg-opacity-50'
                         />
                     </div>
 
