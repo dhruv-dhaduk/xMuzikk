19deb3852c6ec59a3fda1dc87dbd44270ebfaac4
dhruv-dhaduk <dhadukd44@gmail.com>
2024-06-05 15:50:44 +0530

    Wrote the site and developer info in AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -64,8 +64,69 @@ function AboutPage() {
                     </div>
                 )
             }
+
+            <div className='flex justify-center mt-10 p-4'>
+                <div className='w-full max-w-[50rem]'>
+                    <h1 className='mb-2 text-2xl tablet:text-3xl font-bold select-none'>About this site</h1>
+                    <hr className='mt-1 mb-4 border border-[#505050]' />
+
+                    <P>Hello, World !</P>
+                    <P>This is xMuzikk : A music player for the web, where you can listen to your favorite music on your phone, tablet or computer. (Some features are still in development and will be available soon.)</P>
+                    <P>It is built with React, TailwindCSS, Appwrite and YouTube APIs.</P>
+                    <P>
+                        Here is the Source Code link : 
+                        &nbsp;
+                        <a
+                            href='https://github.com/dhruv-dhaduk/xMuzikk'
+                            target='_blank'
+                            className='underline select-all whitespace-nowrap'
+                        >
+                            https://github.com/dhruv-dhaduk/xMuzikk
+                        </a>
+                    </P>
+                    
+                    <P>This is a personal project, and it is only for my learning purposes.</P>
+                    <P>All music and songs belongs to YouTube and their respective content creators. And link to original video on YouTube is provided with every music and song.</P>
+                    
+                </div>
+            </div>
+
+            <div className='flex justify-center p-4'>
+                <div className='w-full max-w-[50rem]'>
+                    <h1 className='mb-2 text-2xl tablet:text-3xl font-bold select-none'>About developer</h1>
+                    <hr className='mt-1 mb-4 border border-[#505050]' />
+
+                    <P>Hello, my name is Dhruv Dhaduk. I am a passionate software engineer from Gujarat, India.</P>
+                    <P>I am currently studying Computer Engineering at the L.D. College of Engineering, Ahmedabad.</P>
+
+                    <ul>
+                        <h2 className='text-xl font-semibold'>
+                            Here are my contact details : 
+                        </h2>
+                        
+                        <li className='select-none indent-8'>
+                            Email : <a href='mailto:dhadukd44@gmail.com' target='_blank' className='underline select-all whitespace-nowrap'>dhadukd44@gmail.com</a>
+                        </li>
+                        <li className='select-none indent-8'>
+                            GitHub : <a href='https://github.com/dhruv-dhaduk' target='_blank' className='underline select-all whitespace-nowrap'>@dhruv-dhaduk</a>
+                        </li>
+                        <li className='select-none indent-8'>
+                            X (Formerly Twitter) : <a href='https://x.com/dhruvdhaduk0' target='_blank' className='underline select-all whitespace-nowrap'>@dhruvdhaduk0</a>
+                        </li>
+                        <li className='select-none indent-8'>
+                            LinkedIn : <a href='https://www.linkedin.com/in/dhruvdhaduk' target='_blank' className='underline select-all whitespace-nowrap'>in/dhruvdhaduk</a>
+                        </li>
+                            
+                    </ul>
+                </div>
+            </div>
+
         </div>
     );
+}
+
+function P({ children, className }) {
+    return <p className={`mx-1 my-4 text-base/5 select-none ${className}`}>{children}</p>;
 }
 
 export default AboutPage;
