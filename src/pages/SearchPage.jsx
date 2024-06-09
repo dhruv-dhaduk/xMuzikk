9c8300e89ba48e026a2241363539bd67f9196eda
dhruv-dhaduk <dhadukd44@gmail.com>
2024-06-09 21:54:54 +0530

    Created the auth firewall in SearchPage

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from 'react';
+
+import { authService } from '../dataManager/AppwriteService.js';
+
+import Spinner from '../components/ui/Spinner.jsx';
+import AuthLinks from '../components/AuthLinks.jsx';
+
 function SearchPage() {
+    const [user, setUser] = useState(undefined);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const { response } = await authService.getAccountDetails();
+
+            if (!response) {
+                setUser(null);
+            }
+            else {
+                setUser(response);
+            }
+        }
+
+        fetchUser();
+    }, []);
+
+    if (user === undefined) {
+        return (
+            <div className='flex justify-center p-4'>
+                <Spinner />
+            </div>
+        );
+    }
+
+    if (user === null) {
+        return (
+            <AuthLinks message='Please Login or Signup to enable this feature' />
+        );
+    }
+
     return (
-        <div className='w-full pt-56'>
-            <p className='text-5xl text-center select-none'>
-                Coming Soon
-            </p>
+        <div>
+            Allright
         </div>
     );
 }
