893ab8d97f2538f08fb95d9823d0499ce50c87e8
dhruv-dhaduk <dhadukd44@gmail.com>
2025-01-12 00:24:48 +0530

    Added interactivity in Side Buttons in Player Page

--- src/components/player/SideButtons.jsx
+++ src/components/player/SideButtons.jsx
@@ -5,21 +5,40 @@ import refreshIcon from '/icons/refresh.svg';
 import heartIcon from '/icons/heart_hollow.svg';
 
 function SideButtons({ videoID, title, hidePlayer, refreshPlayer }) {
+    const shareMusic = () => {
+        if (!navigator.share) return;
+
+        navigator.share({
+            title,
+            url: `/music/${videoID}`,
+        });
+    };
+
+    const openInYouTube = () => {
+        const isAgreed = window.confirm('Play this song in YouTube ?');
+        if (isAgreed)
+            window.open(`https://youtu.be/${videoID}`, '_blank');
+    }
+    
     return (
         <div className='w-12 min-h-full p-1 flex flex-col items-center justify-between'>
             <Btn
+                onClick={hidePlayer}
                 iconSrc={closeIcon}
                 className='bg-white rounded-full bg-opacity-25 p-[6px]'
             />
             <Btn
+                onClick={shareMusic}
                 iconSrc={shareIcon}
                 className='p-[7px]'
             />
             <Btn
+                onClick={openInYouTube}
                 iconSrc={youtubeIcon}
                 className='p-[7px]'
             />
             <Btn
+                onClick={refreshPlayer}
                 iconSrc={refreshIcon}
                 className='p-[7px]'
             />
@@ -34,7 +53,7 @@ function SideButtons({ videoID, title, hidePlayer, refreshPlayer }) {
 function Btn({ iconSrc, className, onClick }) {
     return (
         <button
-            className={`w-full aspect-square ${className}`}
+            className={`w-full aspect-square cursor-pointer active:scale-[0.8] duration-200 ${className}`}
             onClick={onClick}
         >
             <img
